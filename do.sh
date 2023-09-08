afeca2f91fe68cbbcb5d82e15700351598c36cce
lekclc <s2676225006@gmail.com>
2023-09-08 12:38:58 +0800

    2

---
+++ do.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "export PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc && source ~/.bashrc
+sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list && sudo apt update #更换源
+mkdir $HOME/Desktop/pwntools #创建pwntools文件夹
+mkdir $HOME/Desktop/pwntools/gdb #创建gdb文件夹
+list && sudo apt update 
+sudo apt install git
+git clone https://github.com/lekclc/pwn_environment.git
+
+cd pwn_environment
+
+chmod 775 pwn_ubuntu2004_vm.sh #修改权限
+chmod 775 install.sh #修改权限
+chmod 775 zsh.sh #修改权限
+
+bash pwn_ubuntu2004_vm.sh
+bash install.sh
+bash zsh.sh
--- pwn_ubuntu2004_vm.sh
+++ pwn_ubuntu2004_vm.sh
@@ -2,14 +2,12 @@
 #第一个运行
 path=$HOME/Desktop/pwn_environment #修改为当前路径
 
-mkdir $HOME/Desktop/pwntools #创建pwntools文件夹
 #ubuntu 20.04
 psths=$HOME/Desktop #当前路径
 cd $HOME/Desktop/pwntools #进入pwntools文件夹
 #----------------------配置
-echo "export PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc && source ~/.bashrc
+echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc && source $HOME/.bashrc
 #----------------------换源
-sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list && sudo apt update #更换源
 sudo apt install python3-pip  #安装pip 
 python3 -m pip install --upgrade pip -i https://pypi.mirrors.ustc.edu.cn/simple #更新pip
 sudo apt install ruby #安装ruby
@@ -19,9 +17,12 @@ gem sources --remove https://rubygems.org/  #移除默认源
 gem sources -a https://mirrors.ustc.edu.cn/rubygems/  #添加科大源
 gem sources -u  #更新源缓存
 #----------------------pwntools
-echo "export PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc && source ~/.bashrc
+echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc && source $HOME/.bashrc
 sudo apt-get update #更新源
 sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential #安装依赖
+sudo pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
 python3 -m pip install --upgrade pip -i https://pypi.mirrors.ustc.edu.cn/simple #更新pip
 python3 -m pip install --upgrade pwntools -i https://pypi.mirrors.ustc.edu.cn/simple #安装pwntools
 cd $HOME/Desktop/pwntools #进入pwntools文件夹
@@ -29,7 +30,7 @@ touch requirements.txt #创建requirements.txt文件
 echo "wheel==0.30.0" > requirements.txt #写入配置
 echo "docutils" >> requirements.txt 
 echo "breathe==4.7.3" >> requirements.txt
-echo "sphinx>1.8" >> requirements.txt
+echo "sphinx" >> requirements.txt
 echo "docutils==0.14" >> requirements.txt
 echo "sphinx_rtd_theme" >> requirements.txt
 echo "junit2html" >> requirements.txt
@@ -44,32 +45,40 @@ pip3 install --user -r requirements.txt -i https://pypi.mirrors.ustc.edu.cn/simp
 sudo apt install wget #安装wget
 sudo apt install vim #安装vim
 sudo apt install gcc #安装gcc
-sudo apt-get -y install gdb #安装gdb
+sudo apt install g++ #安装g++
 #----------------------
 sudo gem install one_gadget #安装one_gadget
 sudo gem install seccomp-tools #安装seccomp-tools
 sudo apt install checksec #安装checksec
-sudo -H python3 -m pip install ROPgadget #安装ROPgadget
+sudo -H python3 -m pip install ROPgadget -i https://pypi.mirrors.ustc.edu.cn/simple #安装ROPgadget
 sudo apt install patchelf #安装patchelf
 #----------------------
-cd ~/ #进入根目录
-git clone https://github.com/scwuaptx/Pwngdb.git  #下载Pwngdb
-cp ~/Pwngdb/.gdbinit ~/ #复制.gdbinit文件到根目录
-git clone https://github.com/pwndbg/pwndbg #下载pwndbg
-cd pwndbg #进入pwndbg文件夹
-./setup.sh #安装pwndbg
-cd ~/ #进入根目录
-echo "#source ~/peda/peda.py" > ~/.gdbinit #写入配置
-echo "source ~/pwndbg/gdbinit.py" >> ~/.gdbinit
-echo "source ~/Pwngdb/pwngdb.py" >> ~/.gdbinit
-echo "source ~/Pwngdb/angelheap/gdbinit.py" >> ~/.gdbinit
-echo "define hook-run" >> ~/.gdbinit
-echo "python" >> ~/.gdbinit
-echo "import angelheap" >> ~/.gdbinit
-echo "angelheap.init_angelheap()" >> ~/.gdbinit
-echo "end" >> ~/.gdbinit
-echo "end" >> ~/.gdbinit
-echo "~" >> ~/.gdbinit
+#以下gdb安装脚本并不完善
+cd $HOME/Desktop/pwntools/gdb #进入gdb文件夹
+sudo apt remove gdb
+sudo apt install rpm
+rpm -qa | grep gdb
+wget http://ftp.gnu.org/gnu/gdb/gdb-13.2.tar.gz
+tar -zxvf gdb-13.2.tar.gz
+sudo apt install make
+cd gdb-13.2
+./configure
+make
+sudo make install
+cd $HOME/Desktop/pwntools/gdb #进入gdb文件夹
+git clone https://github.com/longld/peda.git
+git clone https://github.com/scwuaptx/Pwngdb.git
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+cd $HOME/Desktop/pwntools/gdb
+cp $HOME/Desktop/pwntools/gdb/Pwngdb/.gdbinit $HOME/.gdbinit
+cd $HOME #进入根目录
+echo "#source $HOME/Desktop/pwntools/gdb/peda/peda.py" > $HOME/.gdbinit #写入配置
+echo "source $HOME/Desktop/pwntools/gdb/pwndbg/gdbinit.py " >> $HOME/.gdbinit #写入配置
+echo "source $HOME/Desktop/pwntools/gdb/Pwngdb/pwngdb.py" >> $HOME/.gdbinit
+echo "source $HOME/Desktop/pwntools/gdb/Pwngdb/angelheap/gdbinit.py" >> $HOME/.gdbinit
+
 #----------------------
 cd $HOME/Desktop/pwntools #进入pwntools文件夹
 git clone https://github.com/lieanu/LibcSearcher.git #下载LibcSearcher
@@ -81,7 +90,4 @@ git clone https://github.com/matrix1001/glibc-all-in-one.git #下载glibc-all-in
 cd $HOME/Desktop/pwn_environment #进入pwn_environment文件夹
 sudo apt install zsh #安装zsh
 chsh -s /bin/zshsh  #修改默认shell
-chmod 775 install.sh #修改权限
-chmod 775 zsh.sh #修改权限
-bash install.sh #运行install.sh
 #----------------------
