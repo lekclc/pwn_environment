b44240c653167da190fa27e344eb4512d339430f
lekclc <s2676225006@gmail.com>
2023-11-11 00:04:15 +0800

    2

--- zsh.sh
+++ zsh.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
 #先运行install.sh
+
 sudo apt install autojump
 git clone https://gitee.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
  git clone https://gitee.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sudo sed -i 's/plugins=(git)/plugins=(git extract autojump zsh-autosuggestions zsh-syntax-highlighting)/g' $HOME/.zshrc
+sudo ln ~/.zshrc /root/.zshrc
+sudo cp -r  ~/.oh-my-zsh /root/
 source $HOME/.zshrc
 echo "export PATH="$PATH:$HOME/.local/bin"" >> $HOME/.zshrc
 source $HOME/.zshrc
