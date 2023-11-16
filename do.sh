e59575e2d89f7f8d4bab82259e048e0a5dc337da
lekclc <s2676225006@gmail.com>
2023-11-16 21:22:19 +0800

    2

--- do.sh
+++ do.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yes|echo "export PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc && source ~/.bashrc
 yes|sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list && sudo apt update #更换源
-yes|mkdir $HOME/pwntools #创建pwntools文件夹
-yes|mkdir $HOME/pwntools/gdb #创建gdb文件夹
+mkdir $HOME/pwntools #创建pwntools文件夹
+mkdir $HOME/pwntools/gdb #创建gdb文件夹
 yes|list && sudo apt update 
 yes|sudo apt install git
 yes|git clone https://github.com/lekclc/pwn_environment.git
